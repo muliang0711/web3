39d551d4c909b99be52753fee342f484d077dc0d
hy <hyplayer001@gmail.com>
2026-02-19 07:56:54 +0800

    Feat : updaet User Register smart contract

---
+++ on-chain/contracts/UserRegistry.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract UserRegistry {
+    // 1. struct :
+    struct User {
+        address wallet;
+        string name;
+        bool isRegistered;
+    }
+
+    // 2.state variable :
+    mapping(address => User) public users;
+
+    // 3. events :
+    event UserRegistered(address indexed userAddress, string username);
+
+    // 4. functions :
+    function register(string memory _name) public {
+        // modifier :
+        require(bytes(_name).length > 0, "Username cannot be empty");
+        require(!users[msg.sender].isRegistered, "User already registered");
+
+        // logic :
+        users[msg.sender] = User(msg.sender, _name, true);
+        emit UserRegistered(msg.sender, _name);
+    }
+
+    function getUser(address _userAddress) public view returns (User memory) {
+        return users[_userAddress];
+    }
+}
