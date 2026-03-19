9be94415e90524ac23c625d58ce3b1c816cd16f2
hy <hyplayer001@gmail.com>
2026-03-19 11:11:18 +0800

    FIxing : correct frontend contarct address

--- client/src/hooks/useCampaignFactory.ts
+++ client/src/hooks/useCampaignFactory.ts
@@ -4,7 +4,7 @@ import { useAccount } from 'wagmi';
 import { parseEther } from 'viem';
 
 // TODO: Update this after deploying CampaignFactory
-const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const FACTORY_ABI = [
     {
--- client/src/hooks/useUserRegistry.ts
+++ client/src/hooks/useUserRegistry.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useReadContract, useWriteContract, useWaitForTransactionReceipt } from 'wagmi';
 import { useAccount } from 'wagmi';
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const CONTRACT_ABI = [
     {
         "type": "function",
